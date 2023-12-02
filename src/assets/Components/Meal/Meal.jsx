969f7ead72e0c6d90b9b8b82bd1d079d61d182bc
mohammed binyamin <binyam0068@gmail.com>
2023-12-02 15:30:19 +0530

    added a filtered layer to select from the catogories

--- src/assets/Components/Meal/Meal.jsx
+++ src/assets/Components/Meal/Meal.jsx
@@ -3,12 +3,14 @@ import { useEffect,useState } from 'react'
 import "./meal.css"
 function Meal() {
 const [meal,setMeal]=useState([])
+const [filtr,setFiltr]=useState([])
+const [loading,setLoading]=useState(true)
 const getData = async()=>{
     try{
       const api_key ="https://www.themealdb.com/api/json/v1/1/search.php?f=c"  
       const response = await fetch(api_key)
       const data = await response.json()
-      console.log(data.meals)
+      
       setMeal(data.meals)
 
     }catch(error){
@@ -18,6 +20,7 @@ const getData = async()=>{
 }
     useEffect(()=>{
     getData()
+    setLoading(false)
     },[])
     
     let mapedItems = meal.map((e,index)=>{
@@ -31,6 +34,39 @@ const getData = async()=>{
                     )
                 }
         
+      
+    })
+
+    const selectionfn =(data)=>{
+        console.log(data)
+    let fileterrr = meal.filter((val)=>{
+        
+        return val.strCategory === data
+    })
+
+    // Added the filtered value to the filter state
+    setFiltr(fileterrr)
+     
+
+     
+   
+    }
+
+    let selection = filtr.map((val,index)=>{
+        return (
+            <li key={index} style={{margin:"10px"}} ><img style={{width:"250px",height:"250px",borderRadius:"10px"}} src={val.strMealThumb} alt="nil"  /></li>
+        )
+    })
+  
+   // maped catogoires
+    let catogories = meal.map((data,index)=>{
+        return(
+            
+            <li onClick={()=>{
+                selectionfn(data.strCategory)
+
+            }} className='ctr-list' >{data.strCategory}</li>
+        )
 
     })
   return (
@@ -42,9 +78,22 @@ const getData = async()=>{
     
     <div  className='feched-data' style={{display:"flex",flexWrap:"wrap",marginTop:"30px",justifyContent:"center"}}>
 
-    {mapedItems}
+     { !loading? mapedItems : <h1>Loading....</h1>} 
+ </div>
+ <div className="list-catog" style={{textAlign:"center"}}>
+ <h1>Search by catogories</h1>
+    <ul style={{display:"flex",flexWrap:"wrap",textDecoration:"none",listStyle:"none",padding:"20px"}}>
+        {!loading &&catogories}
+    </ul>
 </div>
+<div className="selection-list" >
 
+    <ul style={{display:"flex",flexWrap:"wrap",justifyContent:"center",padding:"20px",listStyle:"none",}}>
+       
+        {selection }
+    </ul>
+
+</div>
 </div>
 
 </>
