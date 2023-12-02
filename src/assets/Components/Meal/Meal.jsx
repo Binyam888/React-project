3497cbf492931bdc77707e1d2e0ea52c2bad01f9
mohammed binyamin <binyam0068@gmail.com>
2023-12-02 15:51:05 +0530

    removed inline style to make the jsx page pretty added seperet class necessary elements

--- src/assets/Components/Meal/Meal.jsx
+++ src/assets/Components/Meal/Meal.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { useEffect,useState } from 'react'
 import "./meal.css"
 function Meal() {
-const [meal,setMeal]=useState([])
-const [filtr,setFiltr]=useState([])
-const [loading,setLoading]=useState(true)
+const [meal,setMeal]=useState([])           // feched data from the server
+const [filtr,setFiltr]=useState([])        // filtered value for showing catg-list
+const [loading,setLoading]=useState(true) // conditional redendering
 const getData = async()=>{
     try{
       const api_key ="https://www.themealdb.com/api/json/v1/1/search.php?f=c"  
       const response = await fetch(api_key)
       const data = await response.json()
       
-      setMeal(data.meals)
+      setMeal(data.meals) // added fetched data to meal using setMeal
 
     }catch(error){
         console.error(error)
@@ -45,6 +45,7 @@ const getData = async()=>{
     })
 
     // Added the filtered value to the filter state
+
     setFiltr(fileterrr)
      
 
@@ -54,7 +55,7 @@ const getData = async()=>{
 
     let selection = filtr.map((val,index)=>{
         return (
-            <li key={index} style={{margin:"10px"}} ><img style={{width:"250px",height:"250px",borderRadius:"10px"}} src={val.strMealThumb} alt="nil"  /></li>
+            <li className='selection' key={index}  ><img src={val.strMealThumb} alt="nil"  /></li>
         )
     })
   
@@ -72,23 +73,29 @@ const getData = async()=>{
   return (
 <>
 <div className="container"style={{marginTop:"50px"}} >
-    <div className="texts" style={{textAlign:"center",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
+    <div className="texts" >
     <h1>Special Dishes</h1>
    </div>
     
-    <div  className='feched-data' style={{display:"flex",flexWrap:"wrap",marginTop:"30px",justifyContent:"center"}}>
+    <div  className='feched-data' >
+
+
+     {/* updating the fetched data from the dom */}
+
 
      { !loading? mapedItems : <h1>Loading....</h1>} 
  </div>
- <div className="list-catog" style={{textAlign:"center"}}>
- <h1>Search by catogories</h1>
-    <ul style={{display:"flex",flexWrap:"wrap",textDecoration:"none",listStyle:"none",padding:"20px"}}>
-        {!loading &&catogories}
-    </ul>
-</div>
+    <div className="list-catog" style={{textAlign:"center"}}>
+             <h1>Search by catogories</h1>
+        <ul className='catogry-ul'>
+            {/* updating catogories list in the dom */}
+
+             {!loading &&catogories}
+        </ul>
+    </div>
 <div className="selection-list" >
 
-    <ul style={{display:"flex",flexWrap:"wrap",justifyContent:"center",padding:"20px",listStyle:"none",}}>
+    <ul >
        
         {selection }
     </ul>
