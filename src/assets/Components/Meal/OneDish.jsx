1d659e019c63e6c418446f540d93f8b1ec7efd4c
mohammed binyamin <binyam0068@gmail.com>
2023-12-12 02:20:08 +0530

    created a component for showing only one dish

---
+++ src/assets/Components/Meal/OneDish.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function OneDish(props) {
+    const {singleitem} = props
+   const items= singleitem.map((item)=>{
+        return(
+            
+            <li className='selection'>
+             <img  src={item.strMealThumb} alt="nil" />
+             <h4>{item.strMeal}</h4>
+            </li>
+           
+
+        
+            
+        )
+    })
+  return (
+    <div className='container' >
+        <ul style={{display:"flex",flexWrap:"wrap",listStyle:"none",justifyContent:"center"}}>
+            {items.slice(0,8)}
+        </ul>
+
+    </div>
+  )
+}
+
+export default OneDish
